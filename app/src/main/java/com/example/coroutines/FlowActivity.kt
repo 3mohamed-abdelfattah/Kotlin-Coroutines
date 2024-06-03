c0730b94320b0fbef006798cc0e5a3acf9467c90
MohamedMohamed <m01126165016@icloud.com>
2024-06-03 18:13:39 +0300

    flow intermediate operators

--- app/src/main/java/com/example/coroutines/FlowActivity.kt
+++ app/src/main/java/com/example/coroutines/FlowActivity.kt
@@ -9,7 +9,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
 class FlowActivity : AppCompatActivity() {
@@ -23,25 +25,44 @@ class FlowActivity : AppCompatActivity() {
             insets
         }
 
-        playFlow()
+//        playFlow()
+        playFlowMap()
 
     }
 
-    private fun playFlow() {
-        val flow = flow {
-            for (i in 1..20) {
-                emit("Hello")
-                emit("World!!  $i")
+
+    // Use Filter And Map in flow
+    private fun playFlowMap() {
+        val flow = flow<Int> {
+            for (i in 1..100) {
+                emit(i)
                 delay(1000)
             }
         }
 
         lifecycleScope.launch {
-            flow.collect {
-                Log.d(TAG, "playFlow: $it ")
+            flow.map { it }.filter { it % 2 == 1 }.collect {
+                Log.d(TAG, "playFlowMap: ${it.toString()}")
             }
         }
     }
+
+
+//    private fun playFlow() {
+//        val flow = flow {
+//            for (i in 1..20) {
+//                emit("Hello")
+//                emit("World!!  $i")
+//                delay(1000)
+//            }
+//        }
+//
+//        lifecycleScope.launch {
+//            flow.collect {
+//                Log.d(TAG, "playFlow: $it ")
+//            }
+//        }
+//    }
 
 
 }
